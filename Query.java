49a2f8818d69c8d2d3eeb9f78e6070f10fa4af05
Brett Dispoto <35391361+DispotoBrett@users.noreply.github.com>
2018-12-29 19:02:47 -0800

    Update Query.java

--- Query.java
+++ Query.java
@@ -1,19 +1,55 @@
+import java.io.IOException;
+
+/**
+ * Creates and manages an array of Craigslist listings (For sale).
+ * @author Brett Dispoto 
+ */
 public class Query 
 {
+	CraigslistParser p;
 	Listing[] theListings;
 	
+	/**
+	 * Creates a new query.
+	 * @param location the desiered cl location code, (ex: Los Angeles -> LA).
+	 * @param search the product to search for.
+	 */
 	public Query(String location, String search)
 	{
 		CraigslistParser p = new CraigslistParser(location, search);
-		theListings = p.getInfo(CraigslistParser.GET_ALL_LISTINGS);
+		try 
+		{
+			theListings = p.getInfo(CraigslistParser.MAX_LISTINGS);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 	
+	/**
+	 * Creates a new query.
+	 * @param location the desiered cl location code, (ex: Los Angeles -> LA).
+	 * @param search the product to search for.
+	 * @param numberOfResults the desiered number of results.
+	 */
 	public Query(String location, String search, int numberOfResults)
 	{
-		CraigslistParser p = new CraigslistParser(location, search);
-		theListings = p.getInfo(numberOfResults);	
+		p = new CraigslistParser(location, search);
+		try 
+		{
+			theListings = p.getInfo(numberOfResults);
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}	
 	}
 	
+	/**
+	 * Gets the listings found for the queried term.
+	 * @return the listings found in array from.
+	 */
 	public Listing[] getListings()
 	{
 		return theListings;
