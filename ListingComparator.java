2ba0c6fa5ac5b6f1cca1a1385e8ae84c239aaba4
Brett Dispoto <35391361+DispotoBrett@users.noreply.github.com>
2018-12-29 19:01:53 -0800

    Update ListingComparator.java

--- ListingComparator.java
+++ ListingComparator.java
@@ -1,5 +1,10 @@
 import java.util.Comparator;
 
+/**
+ * Implementation of Comparator interface (JAVA SE8).
+ * @author Brett Dispoto
+ *
+ */
 public class ListingComparator implements Comparator<Listing>
 {
 	public int compare(Listing l1,Listing l2)
